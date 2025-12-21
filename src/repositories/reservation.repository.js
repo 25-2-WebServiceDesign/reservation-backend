456945a6641dfc694e241d5603474cc5d578b5b8
hyeongu01 <cdy5212@gmail.com>
2025-12-22 01:23:23 +0900

    feat: add reservation repository

---
+++ src/repositories/reservation.repository.js
@@ -0,0 +1,35 @@
+const { Reservation } = require('../models');
+
+async function create(data, options = {}) {
+  return Reservation.create(data, options);
+}
+
+async function findById(id, options = {}) {
+  return Reservation.findByPk(id, options);
+}
+
+async function findOne(where = {}, options = {}) {
+  return Reservation.findOne({ where, ...options });
+}
+
+async function findAll(where = {}, options = {}) {
+  return Reservation.findAll({ where, ...options });
+}
+
+async function update(id, data, options = {}) {
+  await Reservation.update(data, { where: { id }, ...options });
+  return findById(id);
+}
+
+async function remove(id, options = {}) {
+  return Reservation.destroy({ where: { id }, ...options });
+}
+
+module.exports = {
+  create,
+  findById,
+  findOne,
+  findAll,
+  update,
+  remove,
+};
