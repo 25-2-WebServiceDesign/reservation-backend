c63b5e29abd6f21525370ce828030021f538706d
hyeongu01 <cdy5212@gmail.com>
2025-12-21 15:58:34 +0900

    feat: add refreshToken repository

---
+++ src/repositories/refreshToken.repository.js
@@ -0,0 +1,35 @@
+const { RefreshToken } = require('../models');
+
+async function create(data, options = {}) {
+  return RefreshToken.create(data, options);
+}
+
+async function findById(id, options = {}) {
+  return RefreshToken.findByPk(id, options);
+}
+
+async function findOne(where = {}, options = {}) {
+  return RefreshToken.findOne({ where, ...options });
+}
+
+async function findAll(where = {}, options = {}) {
+  return RefreshToken.findAll({ where, ...options });
+}
+
+async function update(id, data, options = {}) {
+  await RefreshToken.update(data, { where: { id }, ...options });
+  return findById(id);
+}
+
+async function remove(id, options = {}) {
+  return RefreshToken.destroy({ where: { id }, ...options });
+}
+
+module.exports = {
+  create,
+  findById,
+  findOne,
+  findAll,
+  update,
+  remove,
+};
