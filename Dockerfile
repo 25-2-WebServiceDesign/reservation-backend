346c3595d03f5e40cdb89f66a440f6ef4de04dce
hyeongu01 <cdy5212@gmail.com>
2025-12-26 03:58:58 +0900

    feat: docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM node:20-alpine
 
+# app 디렉터리 생성
 WORKDIR /app
 
+# 앱 의존성 설치
 COPY package*.json ./
 RUN npm ci --omit=dev
 
+# app 소스 추가
 COPY . .
 
 EXPOSE 3000
-
-CMD ["node", "src/app.js"]
+CMD ["node", "src/server.js"]
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,5 @@
 const admin = require("firebase-admin");
+require("dotenv").config()
 // firebase_config.json 없어서 기존 코드 주석처리
 /* const serviceAccount = require("../../firebase_config.json");
 
@@ -11,7 +12,12 @@ module.exports = admin; */
 let firebaseAdmin = null;
 
 try {
-    const serviceAccount = require("../../firebase_config.json");
+    const serviceAccount = JSON.parse(
+        Buffer.from(
+            process.env.FIREBASE_CONFIG_BASE64,
+            "base64"
+        ).toString("utf-8")
+    )
 
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
